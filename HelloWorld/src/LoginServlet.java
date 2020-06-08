2a08e9f56b2600f3853dca6d76eb7998701f246f
Virendra <patelvirendra62@gmail.com>
2020-06-08 14:47:05 +0530

    request

--- HelloWorld/src/LoginServlet.java
+++ HelloWorld/src/LoginServlet.java
@@ -1,17 +1,27 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 public class LoginServlet extends GenericServlet{
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Login Servelt Called");
+		PrintWriter writer =  res.getWriter();
+		writer.write("<button>Click Here</button>");
+		writer.flush();
+		writer.close();
 		
 	}
+	
+	
 
 }
