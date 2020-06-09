94d9d15575ae2d96eacbe20f46b88fd72694b41b
Virendra <patelvirendra62@gmail.com>
2020-06-09 22:52:02 +0530

    HTTP

--- HelloWorld/src/LoginServlet.java
+++ HelloWorld/src/LoginServlet.java
@@ -7,32 +7,53 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends GenericServlet{
-
+public class LoginServlet extends HttpServlet{
+	
+	
 	@Override
-	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		HttpServletRequest request = (HttpServletRequest)req;
-		String method = request.getMethod();
-		
-		System.out.println("Login Servelt Called");
-		String resp = "";
-		
-		if(method.equals("POST")) {
-			resp = "<button>Click Here This Was "
-					+ "POST Request</button>";
-		}
-		else if(method.equals("GET")) {
-			resp = "<button >This was Get Request </button>";
-		}
-		
-		PrintWriter writer =  res.getWriter();
-		writer.write(resp);
-		writer.flush();
-		writer.close();
-		
+		String rs = "<button>Click Here This Was "
+				+ "GET Request HTTP</button>";
+		resp.getWriter().write(rs);
 	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String rs = "<button>Click Here This Was "
+				+ "POST Request HTTP</button>";
+		resp.getWriter().write(rs);
+	}
+	
+	
+
+//	@Override
+//	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		HttpServletRequest request = (HttpServletRequest)req;
+//		String method = request.getMethod();
+//		
+//		System.out.println("Login Servelt Called");
+//		String resp = "";
+//		
+//		if(method.equals("POST")) {
+//			resp = "<button>Click Here This Was "
+//					+ "POST Request HTTP</button>";
+//		}
+//		else if(method.equals("GET")) {
+//			resp = "<button >This was Get Request HTTp</button>";
+//		}
+//		
+//		PrintWriter writer =  res.getWriter();
+//		writer.write(resp);
+//		writer.flush();
+//		writer.close();
+//		
+//	}
 	
 	
 
